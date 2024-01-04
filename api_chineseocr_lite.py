bf017a0a376aebfb32507f3743082a113a0e403c
EricGuo <2364319479@qq.com>
2024-01-05 00:01:32 +0800

    [ChineseOCR]添加Umi-OCR对于ChineseOCR项目的支持，感谢chineseocr_lite作者的贡献

---
+++ api_chineseocr_lite.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import site
+import base64
+from PIL import Image
+from io import BytesIO
+
+# 当前目录
+CurrentDir = os.path.dirname(os.path.abspath(__file__))
+# 依赖包目录
+SitePackages = os.path.join(CurrentDir, "site-packages")
+
+class Api:
+    def __init__(self, globalArgd):
+        self.chineseocr = None
+    
+    # 获取OcrHandle 实例
+    def start(self, argd):
+        if self.chineseocr:
+            return ""
+        site.addsitedir(SitePackages)
+        try:
+
+            class _std:
+                def write(self, e):
+                    print(e)
+            
+            sys.__stdout__ = _std
+            sys.__stderr__ = _std
+
+            from .model import OcrHandle
+            self.chineseocr = OcrHandle()
+            self.short_size = argd["short_size"]
+            return ""
+        except Exception as e:
+            self.chineseocr = None
+            err = str(e)
+            print(err)
+            return f"[Error] Error on loading:{err}"
+    
+    def stop(self):
+        self.chineseocr = None
+    
+    #借用自plugins-P2Tocr,进行了修改
+    def _standardized(self,res):
+        datas = []
+        for item in res:
+            text = item[1].split(" ")[1]
+            accuracy = item[2]
+            position = item[0]
+            datas.append({
+                "text": text,
+                "score": accuracy,
+                "box": position.tolist()
+            })
+        if datas:
+            out = {"code": 100, "data": datas}
+        else:
+            out = {"code": 101, "data": ""}
+        return out
+    
+    def _run(self,img:Image):
+        if not self.chineseocr:
+            res = {"code": 201, "data": "chineseocr not initialized."}
+        else:
+            try:
+                res = self.chineseocr.text_predict(img,self.short_size)
+                res = self._standardized(res)
+            except Exception as e:
+                return {"code": 202, "data": f"chineseocr recognize error:{e}"}
+        return res
+    
+    def runPath(self,imgPath: str):
+        return self._run(Image.open(imgPath))
+    
+    def runBytes(self,imgBytes: bytes):
+        return self._run(Image.open(BytesIO(imgBytes)))
+    
+    def runBase64(self,imgBase64: str):
+        return self._run(Image.open(BytesIO(base64.b64decode(imgBase64))))
