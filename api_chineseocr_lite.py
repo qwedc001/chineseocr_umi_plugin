b31cd256962a9d653152cafbbe1f9fcf76451d66
unknown <2230247019@qq.com>
2024-01-05 11:44:04 +0800

    TODO

--- api_chineseocr_lite.py
+++ api_chineseocr_lite.py
@@ -12,14 +12,24 @@ SitePackages = os.path.join(CurrentDir, "site-packages")
 
 """
 TODO 1 :
+优化git结构。插件git仓库中，应该只存放必须的代码文件，减小git管理的体积。
+如 models 、 site-packages 这些存放二进制文件及第三方库的目录，从git中移除。
+将完整的插件打个压缩包放在github的Release里。
+即：git只存放插件代码，Release存放完整的插件包。
+附：从git记录中永久删除指定目录或文件以释放空间：
+https://www.cnblogs.com/shines77/p/3460274.html
+https://www.jianshu.com/p/d333ab0e6818
+谨慎操作！
+
+TODO 2 :
 在本模块的run方法中，img直接转换为cv2对象，跳过转为PIL的步骤。
 在 model.py 和 dbnet_infer.py 的相应接口中，直接传递cv2对象。
 这样可以去除 PIL → np.asarray(BGR) → RGB 的繁琐转换步骤，减少无谓的开销。
 
-TODO 2 :
+TODO 3 :
 将更多可设置参数添加到面板。如 config.py 中的 angle_detect 之类。
 
-TODO 3 :
+TODO 4 :
 精度问题。chineseocr主页有一些测试图片，它展示的识别结果是正确的，但本插件的结果存在一些误差。
 https://github.com/DayBreak-u/chineseocr_lite/tree/master
 需要验证：是否为参数设置不当导致？是否为onnx模型导致？使用别的方法部署（如pytorch）的结果是否一致？
